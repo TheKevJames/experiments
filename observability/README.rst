c738522bd145899b91b6aaae1a2993143c195979
Kevin James <KevinJames@thekev.in>
2021-06-16 01:37:58 -0700

    docs(o11y): first pass laying out thoughts

--- observability/README.rst
+++ observability/README.rst
@@ -1,34 +1,100 @@
+Observability Demo
+==================
+
+This project defines a sample set of k8s clusters instrumented with metrics
+(Prometheus), logging (Loki), and tracing (Tempo) -- ie. "The Observability
+Stack". It can be deployed to any kubernetes cluster as a demo, but the overall
+goal is to *read through it and understand how to fit it into your own systems*
+-- I've attempted to keep things as straightforward as possible to allow you to
+understand each piece rather than making use of Helm/Jsonnet/Tanka/etc: if
+you're just looking for a simple method to deploy these components, the
+official Helm charts/Operators/whatever-method-you-like would be a good place
+to look.
+
+This demo is divided into two cluster types: application and reporting
+clusters. The idea here being that we should be able to scale each
+independently, maintain high availability of your monitoring infrastructure,
+and keep the impact on your application workloads as lightweight as possible.
+
+Application clusters are those you want to monitor, eg. the existing systems
+you already have if you're jsut getting started with monitoring. These will
+include the bare minimum for ingestion of metrics, logging, and traces, with as
+little actual processing as possible. These should be able to scale to
+arbitrarily many clusters running arbitrarily many applications.
+
+Reporting clusters are the aggregation layer which ingest all your monitoring
+data and let you build dashboards/write queries/generate alerts/etc. These
+should be able to scale independently of your application clusters and should
+allow you to run as many as you'd like in parallel, according to your risk
+appetite.
+
+Your first step should be to read through the
+`sample application <./app/main.py>`_. That'll give you an overview of what
+telemetry we'll get, how we instrument our applications, and some of the high
+level goals.
+
+From there, you should read through the
+`application cluster spec <./appcluster.yaml>`_, which will introduce the
+various methods of capturing and forwarding your telemetry.
+
+Finally, you should read through the
+`reporting cluster spec <./reportcluster.yaml>`_, which will handle all the
+aggregation, reporting, and operations side of things.
+
+To get an understanding of the useability, you can run ``make``. You'll need
+to have docker running and your ``kubectl`` configured to point to a valid
+cluster. I've not attempted to avoid causing issues with existing clusters;
+please use a fresh cluster! ``make clean`` can tear down the changes when
+you're done.
+
+TODO: fix GCS reliance
+
+Once the cluster is running, you can check out ``:3000`` for a Grafana instance
+and explore the data. The user/pass is ``admin:admin``. You shouldn't need to
+access anything else, but you can do so by port-forwarding to a given service
+if you want to poke around. For example, to check out the ``alertmanager`` on
+``appcluster-a``, you can run::
+
+    kubectl port-forward -nappcluster-a-monitoring service/alertmanager 9093:9093
+    # then visit localhost:9093
+
+The above should be everything you need to know to understand what's going on
+here. The rest of this README will discuss more orthogonal topics and design
+decisions.
+
+Why Prometheus?
+---------------
+
+Why Thanos?
+-----------
+
+Why Loki?
+---------
+
+Why Tempo?
+----------
+
+Why Grafana Agent?
+------------------
+
+Other "Why"'s?
+--------------
+
 TODO
-====
+----
 
 * logs:
-  * json parser
-  * handle dynamic ``extra`` values
   * examplars?
   * promtail ready probe fix
 * traces:
   * consider using grafana-agent for prometheus/loki forwarding?
-    * fix: grafana-agent needs to enumerate destinations, so does loki
 * metrics:
   * investigate value of thanos query frontend
   * figure out grafana v8 ngalerts integration given thanos-query proxy usage
   * node-exporter and kube-state-metrics:
     * https://www.metricfire.com/blog/ha-kubernetes-monitoring-using-prometheus-and-thanos
-  * determine ideal resource configs
-  * configure grafana auth
-
-READMEify Me
-------------
-
-Explain ns -> clusters
-What corresponds to what, maybe break apart k8s files
-Full walkthrough and reasoning for tech choices and location placement
-make USER=foo
-include expectations eg. "you're assumed to add things like resource limits"
-
-kubectl port-forward -nappcluster-a-monitoring service/alertmanager 9093:9093
-kubectl port-forward -nreportcluster-a-monitoring service/thanos-ruler 9093:9093
-
-kubectl port-forward -nreportcluster-a-monitoring service/thanos-querier 9090:9090
-
-grafana at :3000
+* docs:
+  * constant casing eg. of Prometheus
+* ensure everything has a scrape config
+* use port names
+* verify everything!
--- observability/app/main.py
+++ observability/app/main.py
@@ -6,76 +6,114 @@ import socket
 import random
 import uuid
 
+# We'll be using the prometheus_client for metrics, the builtin logging library
+# for logging, and the opentelemetry client for tracing. The opentelemetry
+# client promises to replace the former two as well to make it easier to manage
+# all three componenets from one place, but it isn't there quite yet. Once it
+# is, using otel libraries for all three may be better!
 import prometheus_client
 import opentelemetry.sdk.resources
 import opentelemetry.sdk.trace
 import opentelemetry.sdk.trace.export
 import opentelemetry.trace
 from opentelemetry.exporter.otlp.proto.grpc import trace_exporter
-from pythonjsonlogger import jsonlogger
 
 
+# Prometheus requires you to create metrics as follows. Any metrics defined in
+# this way will be made available later on via the builtin metrics server.
+#
+# The latency, failures, and in_flight metrics define the three basic metric
+# kinds: histograms, counters, and gauges. The Prometheus docs are your best
+# bet for learning more:
+# https://prometheus.io/docs/practices
 LATENCY = prometheus_client.Histogram('job_latency_seconds', 'Job latency',
                                       ['stage'])
 FAILURES = prometheus_client.Counter('job_failures_total', 'Job failure count')
 IN_FLIGHT = prometheus_client.Gauge('system_in_flight_total',
                                     'Concurrent jobs')
 
+# TODO: I haven't actually figured out the value of Enum and Info type metrics
+# yet, they seem woefully undocumented. I suspect that once this demo supports
+# examplars these might make more sense.
 STATE = prometheus_client.Enum('job_state', 'Current job state',
                                states=['extract', 'transform', 'load'])
 VERSION = prometheus_client.Info('worker_version', 'Current worker version')
 
 
+# Like the metrics, we must initialize our logger and tracer. Note that I'm
+# using `app.main` here rather than `__name__` by convention; since Python sets
+# `__name__ == '__main__'` for your entrypoint, it can be a good idea to set
+# this explicitly and rely on `__name__` everywhere else in your codebase to
+# ensure everything is consistent.
+# The exact value is irrelevant for purposes of this demo.
 logger = logging.getLogger('app.main')
-tracer = opentelemetry.trace.get_tracer(__name__)
+tracer = opentelemetry.trace.get_tracer('app.main')
 
 
-# These don't seem to work with async methods
+# Theoretically, the Prometheus SDK is meant to support decorators for some
+# common cases, which would have made this demo simpler. In practice, they do
+# not currently support async methods. There are various libs such as
+# prometheus-async to solve this, if you want the convenience of:
 # @IN_FLIGHT.track_inprogress()
 # @FAILURES.count_exceptions()
 async def worker(version: int) -> None:
-    """
-    metrics:
-      count concurrent jobs (gauge)
-      count errors (counter)
-      measure job latency, per state (histogram)
-      worker version & hostname (info)  # TODO: what's an info?
-      job state (enum)  # TODO: what's an enum?
-
-    logging:
-      TODO
-
-    tracing:
-      TODO
-    """
+    # All work will be performed within a parent span named "worker".
     with tracer.start_as_current_span('worker'):
+        # The IN_FLIGHT metric will track the current number of jobs being
+        # actively worked on. We'll decrement this once we're done.
         IN_FLIGHT.inc()
         try:
+            # TODO: see Enum/Info
             VERSION.info({'version': f'x.y.{version}',
                           'host': socket.gethostname()})
 
+            # We'll create a sub-span for each stretch of our sample ETL
+            # process. Since traces can be arbitrarily nested, we will be able
+            # to reference the parent/child/sibling traces as need be.
             with tracer.start_as_current_span('extract'):
+                # We'll be tracking the latency for each step of the process.
+                # Note that some labels are constant (eg. such as the name of
+                # this pod) and some can be added on-the-fly. For those added
+                # at runtime, they must be predefined in the metric config --
+                # in this case, `extract` matches the `['stage']` definition
+                # above. By the end of this method, we'll have defined the
+                # latency metric with three values for this tag (eg.
+                # `latency{stage=extract}`, `latency{stage=transform}`, and
+                # `latency{stage=load}`.
                 with LATENCY.labels('extract').time():
+                    # TODO: see Enum/Info
                     STATE.state('extract')
+
+                    # Generate some random data here to give our metrics some
+                    # flavour when we get to aggregation and exploration.
                     job_id = str(uuid.uuid4())
                     job_kind = random.choice(['foo', 'bar', 'baz'])
+                    # The trace can be fetched directly which lets us, for
+                    # example, log traces which would be interesting to
+                    # investigate. In our demo, everything is interesting!
                     trace = (opentelemetry.trace.get_current_span()
                              .get_span_context().trace_id)
-                    logger.info('starting job',
-                                extra={'id': job_id,
-                                       'kind': job_kind,
-                                       'trace': f'{trace:032x}'})
+                    logger.info('starting job %s:%s with trace %032x',
+                                job_kind, job_id, trace)
+
+                    # Fail randomly 1% of the time. We should see the failure
+                    # rate of tasks in the extract stage match this value later
+                    # on.
                     if random.random() < 0.01:
                         raise Exception('failed to extract')
 
             with tracer.start_as_current_span('transform'):
                 with LATENCY.labels('transform').time():
                     STATE.state('transform')
+                    # Different failure rates for different job types
                     failure_ratio = 0.1 if job_kind == 'foo' else 0.05
                     if random.random() < failure_ratio:
                         await asyncio.sleep(0.1)
                         raise Exception('failed to transform')
                     else:
+                        # Our latency will model a pareto distribution to make
+                        # debugging fun. This puts the 90th percentile just
+                        # below 10s.
                         await asyncio.sleep(random.paretovariate(1))
 
             with tracer.start_as_current_span('load'):
@@ -84,12 +122,17 @@ async def worker(version: int) -> None:
                     if random.random() < 0.02:
                         raise Exception('failed to load')
         except Exception:
+            # We'll count failures without attaching a label for the failure
+            # cause: later, we'll be able to use metric->trace exemplars to
+            # determine this value.
             FAILURES.inc()
             raise
         finally:
             IN_FLIGHT.dec()
 
 
+# Nothing to see here, just a simple way to catch k8s exit signals and shutdown
+# gracefully.
 class Shutdown:
     quit = False
 
@@ -97,18 +140,15 @@ class Shutdown:
         signal.signal(signal.SIGINT, self.trigger)
         signal.signal(signal.SIGTERM, self.trigger)
 
-    def trigger(self):
+    def trigger(self, _signo, _frame):
         self.quit = True
 
 
 async def main() -> None:
-    # start prometheus server -- this is in a thread, will asyncio have issues?
-    prometheus_client.start_http_server(8080)
-
-    # graceful shutdown handler for k8s SIGTERMs etc
     shutdown = Shutdown()
 
     try:
+        # TODO: see Enum/Info
         version = 1
         while not shutdown.quit:
             asyncio.create_task(worker(version))
@@ -120,7 +160,7 @@ async def main() -> None:
     except Exception:
         logger.exception('got unhandled exception')
     finally:
-        # generic asyncio graceful shutdown code
+        # More irrelevant and generic asyncio graceful shutdown code.
         tasks = [t for t in asyncio.all_tasks()
                  if t is not asyncio.current_task()]
         if not tasks:
@@ -131,18 +171,37 @@ async def main() -> None:
 
 
 if __name__ == '__main__':
-    logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
-    logHandler.setFormatter(formatter)
-    logger.addHandler(logHandler)
-    logger.setLevel(logging.INFO)
+    # This encapsulates the basic library vs application divide: your library
+    # code will describe what logs/metrics/etc to emit and your application
+    # will describe what to do with those things. In this case, we'll keep it
+    # simple:
 
+    # Initialize logging without any special configs. Loki will need to know
+    # how to scrape your logs (ie. what format they emit) and supports
+    # arbitrary formats... but it supports the default logging format with no
+    # special config, so that'll make our lives easiest.
+    # These will just get sent to stdout, which Promtail knows how to scrape
+    # and can forward along to Loki.
+    logging.basicConfig(level=logging.INFO)
+
+    # Start the builtin Prometheus metrics server. This will expose our app's
+    # metrics on port 8080, which we'll scrape later via Prometheus instances
+    # in our cluster which will forward those metrics along to Thanos/long-term
+    # storage.
+    prometheus_client.start_http_server(8080)
+
+    # Initialize the OpenTelemetry tracing SDK to push traces to the Grafana
+    # Agent, which will in turn forward them to Tempo.
+    # Note that while your metrics and logs are pulled from your application,
+    # traces will be pushed. This seems to be a deliberate design decision in
+    # all trace clients I've played with.
     provider = opentelemetry.sdk.trace.TracerProvider(
         resource=opentelemetry.sdk.resources.Resource(attributes={
             opentelemetry.sdk.resources.SERVICE_NAME: 'app',
         }))
-    # N.B. the namespace would be constant if not for the cluster->namespace
-    # hack used to test multi-cluster
+    # N.B. the namespace would be constant here if not for the
+    # cluster->namespace hack used to run the "multi-cluster" example on a
+    # single cluster.
     namespace = os.environ['POD_NAMESPACE'].replace('default', 'monitoring')
     provider.add_span_processor(
         opentelemetry.sdk.trace.export.BatchSpanProcessor(
