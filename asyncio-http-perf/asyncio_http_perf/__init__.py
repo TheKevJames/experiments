ce0625446551b95cc24a6261da7bb8e440de14e0
Kevin James <KevinJames@thekev.in>
2022-10-21 15:23:54 +0100

    feat(asyncio-http-pref): bump results for newer releases

--- asyncio-http-perf/asyncio_http_perf/__init__.py
+++ asyncio-http-perf/asyncio_http_perf/__init__.py
@@ -1,11 +1,9 @@
 import typer
 
 from . import aiohttp
-from . import asks
 from . import httpx
 
 
 app = typer.Typer()
 app.add_typer(aiohttp.app, name='aiohttp')
-app.add_typer(asks.app, name='asks')
 app.add_typer(httpx.app, name='httpx')
